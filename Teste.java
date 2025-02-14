44d1b1a849220c565c580c771e02036cdc27b9b6
Enzo Senatori <10427175@educa.mackenzie.br>
2025-02-14 16:46:39 -0300

    alteração 1 (Hello World!)

--- Teste.java
+++ Teste.java
@@ -1,1 +1,6 @@
-//teste
+import java.util.*;
+public class Teste{
+    public static void(String[] args){ // arquivo que será executado primeiro. É obrigatório ter este método, sendo parecido com o "int main() em C".
+        System.out.println("Hello World!"); // semelhante ao printf, na linguagem C. Em java, sempre println - "ln" vai pular uma linha.
+    }
+}
